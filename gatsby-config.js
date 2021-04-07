93d763f37e992a968582ade160bf096b858ea159
binarytrance <ganeshan.dash@gmail.com>
2021-04-08 04:35:56 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteMetadata: {
+    title: "ganeshandash.com-FE",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "8hbd7a4c",
+        dataset: "",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
