7198186e160939c941d6f4250642c3273ddf4fc1
binarytrance <ganeshan.dash@gmail.com>
2021-06-21 19:21:36 +0530

    new neutral color

--- src/styles/utils/themes.js
+++ src/styles/utils/themes.js
@@ -3,5 +3,5 @@ import { brown, green, white, blue } from './colors';
 export const baseTheme = {
   siteBackground: brown[200],
   text: blue[100],
-  neutral: white[300],
+  neutral: white[100],
 };
