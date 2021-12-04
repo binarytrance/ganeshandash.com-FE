1cac870d4cd2d7a59d4d7034cc70a9521e5ab95e
binarytrance <ganeshan.dash@gmail.com>
2021-12-05 02:42:50 +0530

    esm files of gatsby node and config

---
+++ gatsby-config.esm.js
@@ -0,0 +1,47 @@
+// coment
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: 'Ganeshan Dash',
+    siteUrl: 'https://ganeshandash.com',
+    description: 'The jack of all trades!',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/assets/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
+    },
+  ],
+};
---
+++ gatsby-node.esm.js
@@ -0,0 +1,63 @@
+import path from 'path';
+
+async function turnTagsIntoPages({ graphql, actions }) {
+  // get a template for this page
+  const tagsTemplate = path.resolve('./src/pages/writer.js');
+  // query all articles related to this tag
+  const { data } = await graphql(`
+    query {
+      allTags: allSanityArticleTags {
+        nodes {
+          id
+          name
+        }
+      }
+    }
+  `);
+  data.allTags.nodes.forEach((tag) => {
+    actions.createPage({
+      path: `tag/${tag.name}`,
+      component: tagsTemplate,
+      context: {
+        tagName: tag.name,
+        tagRegex: `/${tag.name}/i`,
+      },
+    });
+  });
+}
+
+async function turnArticlesIntoPages({ graphql, actions }) {
+  const template = path.resolve('./src/templates/article.js');
+
+  const { data } = await graphql(`
+    query {
+      allArticles: allSanityWriter {
+        nodes {
+          id
+          title
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  data.allArticles.nodes.forEach((article) => {
+    actions.createPage({
+      path: `writer/article/${article.slug.current}`,
+      component: template,
+      context: {
+        slug: article.slug.current,
+        id: article.id,
+        titleRegex: `/${article.title}/i`,
+      },
+    });
+  });
+}
+
+export async function createPages(params) {
+  // 1. tags
+  // 2. article details
+
+  await Promise.all([turnTagsIntoPages(params), turnArticlesIntoPages(params)]);
+}
